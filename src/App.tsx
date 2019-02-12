7d0d8adb473dbaaae39431491ca1576024009b1c
VinceOPS <vince.g.135@gmail.com>
2019-02-12 16:48:54 +0100

    style(tsx): lint files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+
 import LoginButton from './components/LoginButton';
 
 const App = () => {
-    return <LoginButton/>
-}
+    return <LoginButton/>;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
-import ReactDom from 'react-dom';
 import React from 'react';
+import ReactDom from 'react-dom';
+
 import App from './App';
 
 const main = () => {
     const root = document.getElementById('root');
     ReactDom.render(<App/>, root);
-}
+};
+
 main();
