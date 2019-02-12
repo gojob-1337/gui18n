6a8c135cfb53c590e757e0802731e51483e7f4ff
Timur Rustamov <timur.rustamov@epitech.eu>
2019-02-12 15:57:49 +0100

    feat(connect): bootstrap project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+const App = () => {
+    return <div>It works on my machine</div>
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as ReactDom from 'react-dom';
+import * as React from 'react';
+import App from './App';
+
+const main = () => {
+    const root = document.getElementById('root');
+    ReactDom.render(<App/>, root);
+}
+main();
